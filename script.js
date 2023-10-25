8a2275995c63ed15f17286e6df2902bd0f4c7317
rtgiacomelli <rortimoteo@gmail.com>
2023-10-25 15:42:46 -0300

    Changing all links to my own ones

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
 
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-rodolfo-light.png")
   } else {
     //se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-rodolfo.png")
   }
 }
